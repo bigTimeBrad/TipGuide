6b7bc88cae393831816ed472506007943390936a
Melanie Shaffer <melanie.shaffer@gmail.com>
2018-05-30 08:00:11 -0700

    Added three tabs to the main activity

---
+++ app/src/main/java/com/example/tipguide/CalculatorFragment.java
@@ -0,0 +1,16 @@
+package com.example.tipguide;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalculatorFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.item_calculator, null);
+    }
+}
+
