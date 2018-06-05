6fb1622f6edfdfee78b2d09e42215dbea053355d
bigTimeBrad <bradanissen@gmail.com>
2018-06-04 21:12:48 -0700

    fixing a broken test

--- app/src/androidTest/java/com/example/tipguide/MainActivityTest.java
+++ app/src/androidTest/java/com/example/tipguide/MainActivityTest.java
@@ -5,6 +5,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.matcher.ViewMatchers;
@@ -91,20 +92,39 @@ public class MainActivityTest {
         onView(withId(R.id.bill_value)).perform(typeText("100"));
 
         //tip percent
-        onView(withId(R.id.seekBar)).perform(click());
+        onView(withId(R.id.seekBar)).perform(setProgress(20));
+        Espresso.closeSoftKeyboard();
 
         //split
-        onView(withId(R.id.seekBar_one)).perform(click());
+        onView(withId(R.id.seekBar_one)).perform(setProgress(1));
+        Espresso.closeSoftKeyboard();
 
         onView(withId(R.id.calculate_tips)).perform(click());
 
-        onView(withId(R.id.total_to_pay_result)).check(matches(withText("150")));
+        onView(withId(R.id.total_to_pay_result)).check(matches(withText("120")));
 
-        onView(withId(R.id.total_tip_result)).check(matches(withText("50")));
+        onView(withId(R.id.total_tip_result)).check(matches(withText("20")));
 
-        onView(withId(R.id.tip_per_person_result)).check(matches(withText("10")));
+        onView(withId(R.id.tip_per_person_result)).check(matches(withText("20")));
 
 
+    }
+    public static ViewAction setProgress(int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                SeekBar seekBar = (SeekBar) view;
+                seekBar.setProgress(progress);
+            }
+            @Override
+            public String getDescription() {
+                return "Set a progress on a SeekBar";
+            }
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(SeekBar.class);
+            }
+        };
     }
 
 
