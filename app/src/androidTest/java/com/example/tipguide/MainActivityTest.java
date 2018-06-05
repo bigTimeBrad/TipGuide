0c522a1c2059d2316e0759645553521750c00de1
bigTimeBrad <bradanissen@gmail.com>
2018-06-04 19:23:17 -0700

    added some tests on the homepage

--- app/src/androidTest/java/com/example/tipguide/MainActivityTest.java
+++ app/src/androidTest/java/com/example/tipguide/MainActivityTest.java
@@ -5,11 +5,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import android.support.test.runner.AndroidJUnit4;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.startsWith;
+
 import android.support.test.rule.ActivityTestRule;
 
 
@@ -32,4 +38,20 @@ public class MainActivityTest {
         onView(withId(R.id.viewpager)).perform(swipeLeft());
         onView(withText("CALCULATOR")).check(matches(withText("CALCULATOR")));
     }
+
+    @Test
+    public void homePageSpinner(){
+        //onView(withText("HOME")).check(matches(withText("HOME")));
+
+        onView(withText("-------")).perform(click());
+
+        onData(hasToString(startsWith("Morocco")))
+                .perform(click());
+
+        onView(withId(R.id.update))
+                .perform(click());
+        
+    }
+
+
 }
